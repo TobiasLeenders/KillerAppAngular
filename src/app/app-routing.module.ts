731b4ac24142f9f5790428e98f0257f067580f0e
Leenders,Tobias T.A.J <369881@student.fontys.nl>
2018-11-12 00:35:55 +0100

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import {RouterModule, Routes} from '@angular/router';
+import {IndexComponent} from './components/index/index.component';
+import {NgModule} from '@angular/core';
+
+const routes: Routes = [
+  { path: '', component: IndexComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
